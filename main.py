4c2a84b3ae0c8daab06ecb378e861a5ac6e738bd
axjp < 63340001+heapframe@users.noreply.github.com>
2025-06-04 18:19:34 +0100

    quick fix

--- main.py
+++ main.py
@@ -116,47 +116,6 @@ if not hwIdSelected in HwDataList:
             Success = True
 
 
-
-def addGameScore(token, moduleUid, gameType, homeworkUid, gameUid, isTest, correctVocabs, incorrectVocabs, isSentence, featureUid,
-                 isALevel, isVerb, isGrammar, isExam, timeStamp, vocabNumber, correctStudentAns, languagenutTimeMarker,
-                 lastLanguagenutTimeMarker):
-    headers = {
-        'Accept': 'text/plain, */*; q=0.01',
-        'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
-        'Connection': 'keep-alive',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'Origin': 'https://www.languagenut.com',
-        'Referer': 'https://www.languagenut.com/',
-        'Sec-Fetch-Dest': 'empty',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Site': 'same-site',
-        'Sec-GPC': '1',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
-        'sec-ch-ua': '"Not A(Brand";v="99", "Brave";v="121", "Chromium";v="121"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Linux"',
-    }
-
-    params = {
-        'cacheBreaker': str(unixMillis()),
-    }
-
-    #print(moduleUid, gameType, homeworkUid, gameUid, isTest, correctVocabs, incorrectVocabs, isSentence, featureUid,
-    #        isALevel, isVerb, isGrammar, isExam, timeStamp, vocabNumber, correctStudentAns, languagenutTimeMarker,
-    #        lastLanguagenutTimeMarker)
-
-    data = "moduleUid=" + moduleUid + "&gameUid=" + gameUid + "&gameType=" + gameType + "&isTest=" + isTest + "&toietf=de&fromietf=en-GB&score=2000&correctVocabs=" \
-        + correctVocabs + "&incorrectVocabs=" + incorrectVocabs + "&homeworkUid=" + homeworkUid + "&featureUid=" + featureUid + "&isSentence=" + isSentence \
-        + "&isALevel=" + isALevel + "&isVerb=" + isVerb + "&grammarCatalogUid=" + moduleUid + "&isGrammar=" + isGrammar + "&isExam=" + isExam + "&timeStamp=" \
-        + timeStamp + "&vocabNumber=" + vocabNumber + "&rel_module_uid=" + moduleUid + "&dontStoreStats=true&correctStudentAns=&incorrectStudentAns=&product=secondary\
-      &languagenutTimeMarker=" + languagenutTimeMarker + "&lastLanguagenutTimeMarker=" + lastLanguagenutTimeMarker + "&apiVersion=9&token=" + token
-
-    response = requests.post('https://api.languagenut.com/gameDataController/addGameScore', params=params,
-                             headers=headers, data=data).json()
-    token = response.get("newToken")
-    return response
-
-
 work = 0
 taskC = 0
 
